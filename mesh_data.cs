dc2a65f179c815e2614339c0be28ddd860385389
Hüseyin <67561119+findthetrue@users.noreply.github.com>
2021-09-08 11:40:49 -0700

    Update mesh_data.cs

--- mesh_data.cs
+++ mesh_data.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class mesh_data 
-{
+{  //we define vertices of cube
     public static Vector3[] vertices = new Vector3[]
     {
         new Vector3(1,1,1),
@@ -16,6 +16,7 @@ public static class mesh_data
         new Vector3(-1,-1,-1)
 
     };
+    //We use quad method for make 3d cube that reason first we create array of quad like we did for triangles
     public static int[][] facetriangles =
     {
         new int[]{0,1,2,3},
@@ -25,6 +26,7 @@ public static class mesh_data
         new int[]{5,4,1,0},
         new int[]{3,2,7,6}
     };
+    //in this function access given index for 4 vector3 vector and returning it
     public static Vector3[] facevertices(int direction)
     {
         Vector3[] tm = new Vector3[4];
