d6ec82f0ab60a2a91d39690002d48a2e1895f292
Hüseyin <67561119+findthetrue@users.noreply.github.com>
2021-09-08 11:40:28 -0700

    Update script_of_cube.cs

--- script_of_cube.cs
+++ script_of_cube.cs
@@ -7,7 +7,7 @@ public class script_of_cube : MonoBehaviour
 {
     Mesh mesh;
     List<Vector3> vertices;
-    List<int> triangles;
+    List<int> triangles;//unknown size
 
     private void Awake()
     {
@@ -20,6 +20,8 @@ public class script_of_cube : MonoBehaviour
         MakeCube();
         UpdateMesh();
     }
+    //we declare after define the ver and tri but not as array cause of we dont know how many variable we deal with
+    //call the makeface for 6 times 
     void MakeCube()
     {
         vertices = new List<Vector3>();
@@ -30,17 +32,24 @@ public class script_of_cube : MonoBehaviour
             MakeFace(i);
         }
     }
+    int a = 0;
+    //taking information of face quad order 
+    // add range useful for adding every call to new var
+    //we store the count info;
+    //after we arrange these to draw triangles
     void MakeFace(int dir)
     {
         vertices.AddRange(mesh_data.facevertices(dir));
         int vcount = vertices.Count;
-
+        print(vcount);
+        print(a);
         triangles.Add(vcount - 4+0);
         triangles.Add(vcount - 4+1);
         triangles.Add(vcount - 4+2);
         triangles.Add(vcount - 4+0);
         triangles.Add(vcount - 4+2);
         triangles.Add(vcount - 4+3);
+        a++;
     }
     void UpdateMesh()
     {
